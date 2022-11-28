3b354fa56dfe775b4b701059c410c5377436ed7c
mukliang <mukliang@163.com>
2022-11-29 02:00:04 +0800

    postcss

--- 02_Learn_Webpack/03_webpack_loader/webpack.config.js
+++ 02_Learn_Webpack/03_webpack_loader/webpack.config.js
@@ -10,13 +10,12 @@ module.exports = {
     rules: [ 
       { 
         test: /\.css$/, 
-        use: ["style-loader", "css-loader"] 
+        use: ["style-loader", "css-loader", "postcss-loader"] 
       },
       { 
         test: /\.less$/, 
         use: ["style-loader", "css-loader", "less-loader"] 
       },
-      
     ] 
   }
 }
