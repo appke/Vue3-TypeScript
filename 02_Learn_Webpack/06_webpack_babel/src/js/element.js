92e9f070a42b7479e0af1e71e7668aeb13b434b6
mukliang <mukliang@163.com>
2022-11-30 15:50:47 +0800

    ES6语法转换

---
+++ 02_Learn_Webpack/06_webpack_babel/src/js/element.js
@@ -0,0 +1,33 @@
+import "../css/style.css";
+import "../css/title.less";
+import "../css/image.css";
+import "../font/iconfont.css"
+
+const divEl = document.createElement("div");
+divEl.className = "title";
+divEl.innerHTML = "你好啊,李银河";
+
+// 开发错误信息
+const content = "Hello Vue"
+console.log(content.length);
+
+// 设置背景图片
+const bgDivEl = document.createElement("div");
+bgDivEl.className = "image-bg";
+
+// 把图片当成模块引入
+import pngImage from "../img/zznh.png";
+const imgDivEl = document.createElement("img");
+// imgDivEl.src = "../img/zznh.png"; //仅仅赋值路径字符串
+imgDivEl.src = pngImage;
+
+// 插入字体图标
+const iEl = document.createElement("i");
+iEl.className = "iconfont icon-ashbin";
+
+
+document.body.appendChild(divEl);
+document.body.appendChild(bgDivEl);
+document.body.appendChild(imgDivEl);
+document.body.appendChild(iEl);
+
---
+++ 02_Learn_Webpack/06_webpack_babel/src/js/info.js
@@ -0,0 +1,3 @@
+// ES6模块化导出 
+export var name = "liang"
+export var age = 29
