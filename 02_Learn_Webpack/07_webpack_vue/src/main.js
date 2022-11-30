fadfc76fd037f27ec0c84076a9f33c415acdef72
mukliang <mukliang@163.com>
2022-11-30 16:02:28 +0800

    import vue

---
+++ 02_Learn_Webpack/07_webpack_vue/src/main.js
@@ -0,0 +1,21 @@
+// CommonJS模块化导入 
+const {add, mul} = require('./js/math');
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+// ES6模块化导入
+import {name, age} from './js/info';
+console.log('name --- ', name);
+console.log('age  --- ', age);
+
+// 导入js文件
+import "./js/element"
+
+// ES6语法
+const message = "Hello World";
+const names = ["abc", "nba", "cba"];
+names.forEach(item => console.log(item));
+console.log(message);
+
+// 引入vue
+import { createApp } from 'vue';
---
+++ 02_Learn_Webpack/07_webpack_vue/test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var message = "Hello World";
+var names = ["abc", "nba", "cba"];
+names.forEach(function (item) {
+  return console.log(item);
+});
+console.log(message);
