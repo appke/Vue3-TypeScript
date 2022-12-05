f34ba4ad2a0db712ebe908347e799dc4831ccc81
mukliang <mukliang@163.com>
2022-12-05 19:35:57 +0800

    父子控件案例

---
+++ 04_Learn_Component/src/05_组件通信练习/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <tab-bar :titles=titles @titleClick="itemClick"></tab-bar>
+    <h2>{{contents[currentIndex]}}</h2>
+  </div>
+</template>
+
+<script>
+import  TabBar from "./TarBar.vue";
+  export default {
+    components: {
+      TabBar,
+    },
+    data() {
+      return {
+        titles: ["衣服", "鞋子", "裤子"],
+        contents: ["衣服的页面","鞋子的页面","裤子的页面"],
+        currentIndex: 0
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 04_Learn_Component/src/05_组件通信练习/TarBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-bar-item"
+        :class="{active: currentIndex==index}"
+        v-for="(title, index) in titles" 
+        :key="title">
+      <span @click="itemClick(index)">{{title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    emits: ["titleClick"],
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return [];
+        }
+        // ["新闻", "体育", "军事"]
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0,
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index;
+        this.$emit("titleClick", index);
+      }
+    }
+    
+  }
+</script>
+
+<style scoped>
+.tab-bar {
+  display: flex;
+}
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+}
+
+.tab-bar-item.active {
+  color: red;
+}
+
+.tab-bar-item.active span{
+  border-bottom: 5px solid red;
+  padding: 5px 10px;
+}
+
+</style>
