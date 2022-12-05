cb16efc4f21ccec346bd2b04483547d53a78ece5
mukliang <mukliang@163.com>
2022-12-05 17:35:10 +0800

    父传子

---
+++ 04_Learn_Component/src/03_父组件传子/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ShowMessage title="123" content="哈哈哈"></ShowMessage>
+    <ShowMessage title="456" content="嘿嘿"></ShowMessage>
+  </div>
+</template>
+
+<script>
+import ShowMessage from './ShowMessage.vue';
+export default {
+  components: {
+    ShowMessage,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- 04_Learn_Component/src/main.js
+++ 04_Learn_Component/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 // import App from "./01_组件拆分与嵌套/App";
-import App from "./02_组件css作用域/App";
+// import App from "./02_组件css作用域/App";
+import App from "./03_父组件传子/App"
 
 createApp(App).mount('#app')
