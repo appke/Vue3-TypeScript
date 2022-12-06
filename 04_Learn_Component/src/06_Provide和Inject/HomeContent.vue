0f5eddd43366a529e7adfa136f7c014d745b0c1f
mukliang <mukliang@163.com>
2022-12-06 18:33:37 +0800

    provide-inject

---
+++ 04_Learn_Component/src/06_Provide和Inject/HomeContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>06_Provide和Inject</h2>
+    <p>{{name}}-{{age}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    inject: ["name", "age"],
+  }
+</script>
+
+<style scoped>
+
+</style>
--- 04_Learn_Component/src/main.js
+++ 04_Learn_Component/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 // import App from "./02_组件css作用域/App";
 // import App from "./03_父组件传子/App"
 // import App from "./04_子组件传父/App"
-import App from "./05_组件通信练习/App"
+// import App from "./05_组件通信练习/App"
+import App from "./06_Provide和Inject/App"
 
 createApp(App).mount('#app')
