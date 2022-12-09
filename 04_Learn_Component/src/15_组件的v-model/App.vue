4628cbe133410f211aeec2a59fa23df401814d51
mukliang <mukliang@163.com>
2022-12-10 05:45:59 +0800

    计算属性v-model

--- 04_Learn_Component/src/15_组件的v-model/App.vue
+++ 04_Learn_Component/src/15_组件的v-model/App.vue
@@ -6,16 +6,21 @@
 
     <mk-input2 v-model="message"></mk-input2>
     <h2>App的message: {{message}}</h2>
+
+    <mk-input3 v-model="message"></mk-input3>
+    <h2>App的message: {{message}}</h2>
   </div>
 </template>
 
 <script>
 import MkInput from "./MkInput.vue";
 import MkInput2 from "./MkInput2.vue";
+import MkInput3 from "./MkInput3.vue";
   export default {
     components: {
       MkInput,
-      MkInput2
+      MkInput2,
+      MkInput3
     },
     data() {
       return {
---
+++ 04_Learn_Component/src/15_组件的v-model/MkInput3.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model="value">
+    <h2>Mk3内部的message: {{ modelValue }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      set(event) {
+        this.$emit("update:modelValue", event)
+      },
+      get() {
+        return this.modelValue;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
