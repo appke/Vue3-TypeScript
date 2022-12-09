3ac0b6c3dabc78d37321094593e9933b04d4526d
mukliang <mukliang@163.com>
2022-12-09 19:01:35 +0800

    defineAsyncComponent工厂函数

---
+++ 04_Learn_Component/src/12_异步组件的使用/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <home></home>
+    <asyncCategory></asyncCategory>
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import Home from "./Home.vue"
+// import AsyncCategory from "./AsyncCategory.vue";
+const AsyncCategory = defineAsyncComponent(() => import("./AsyncCategory.vue"))
+
+export default {
+  components: {
+    Home,
+    AsyncCategory,
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 04_Learn_Component/src/12_异步组件的使用/AsyncCategory.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    异步组件AsyncCategory组件
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
